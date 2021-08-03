e56e2288c87fd0b43342224d1a8d6a1543a43ca9
kakashi <nk29time@gmail.com>
2021-08-03 09:48:18 +0900

    first

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// 決済ボタンを表示するページ
+Route::get('/', 'PaymentsController@index')->name('index');
+
+// Stripeの処理
+Route::post('/payment', 'PaymentsController@payment')->name('payment');
+
+// 決済完了ページ
+Route::get('/complete', 'PaymentsController@complete')->name('complete');
